89f626e9ed90d313450569f10892c5ddd7aa389a
ArtemkinM <artemkinm@gmail.com>
2025-10-27 18:43:59 +0300

    Добавьте файлы проекта.

---
+++ Pract03.05/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using Pract03._05;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pract03._05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            rbThreePairs.IsChecked = true;
+        }
+
+        private void Summ_Click(object sender, RoutedEventArgs e)
+        {
+            if (rbTwoPairs.IsChecked == true)
+            {
+                if (Int32.TryParse(tbX1.Text, out int x1) == true && Int32.TryParse(tbX2.Text, out int x2) == true && Int32.TryParse(tbY1.Text, out int y1) == true && Int32.TryParse(tbY2.Text, out int y2) == true)
+                {
+                    Pair pair1 = new Pair(x1, x2);
+                    Pair pair2 = new Pair(y1, y2);
+                    Pair pair3 = new Pair();
+                    pair3 = Pair.Sum(pair1, pair2);
+                    tbRez1.Text = Convert.ToString(pair3.A);
+                    tbRez2.Text = Convert.ToString(pair3.B);
+                }
+                else MessageBox.Show("!!!Введите удобоваримые значения!!!");
+            }
+            else if (rbThreePairs.IsChecked == true)
+            {
+                if (Int32.TryParse(tbX1.Text, out int x1) == true && Int32.TryParse(tbX2.Text, out int x2) == true && Int32.TryParse(tbY1.Text, out int y1) == true && Int32.TryParse(tbY2.Text, out int y2) == true && Int32.TryParse(tbZ1.Text, out int z1) == true && Int32.TryParse(tbZ2.Text, out int z2) == true)
+                {
+                    Pair pair1 = new Pair(x1, x2);
+                    Pair pair2 = new Pair(y1, y2);
+                    Pair pair3 = new Pair(z1, z2);
+                    Pair pair4 = new Pair();
+                    pair4 = Pair.Sum(pair1, pair2, pair3);
+                    tbRez1.Text = Convert.ToString(pair4.A);
+                    tbRez2.Text = Convert.ToString(pair4.B);
+                }
+                else MessageBox.Show("!!!Введите удобоваримые значения!!!");
+            }
+        }
+
+        private void OnePlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (rbTwoPairs.IsChecked == true)
+            {
+                if (Int32.TryParse(tbX1.Text, out int x1) == true && Int32.TryParse(tbX2.Text, out int x2) == true && Int32.TryParse(tbY1.Text, out int y1) == true && Int32.TryParse(tbY2.Text, out int y2) == true)
+                {
+                    Pair pair1 = new Pair(x1, x2);
+                    Pair pair2 = new Pair(y1, y2);
+                    Pair.AddOne(ref pair1);
+                    Pair.AddOne(ref pair2);
+                    tbX1.Text = Convert.ToString(pair1.A);
+                    tbX2.Text = Convert.ToString(pair1.B);
+                    tbY1.Text = Convert.ToString(pair2.A);
+                    tbY2.Text = Convert.ToString(pair2.B);
+                }
+                else MessageBox.Show("!!!Введите удобоваримые значения!!!");
+            }
+            else if (rbThreePairs.IsChecked == true)
+            {
+                if (Int32.TryParse(tbX1.Text, out int x1) == true && Int32.TryParse(tbX2.Text, out int x2) == true && Int32.TryParse(tbY1.Text, out int y1) == true && Int32.TryParse(tbY2.Text, out int y2) == true && Int32.TryParse(tbZ1.Text, out int z1) == true && Int32.TryParse(tbZ2.Text, out int z2) == true)
+                {
+                    Pair pair1 = new Pair(x1, x2);
+                    Pair pair2 = new Pair(y1, y2);
+                    Pair pair3 = new Pair(z1, z2);
+                    Pair.AddOne(ref pair1);
+                    Pair.AddOne(ref pair2);
+                    Pair.AddOne(ref pair3);
+                    tbX1.Text = Convert.ToString(pair1.A);
+                    tbX2.Text = Convert.ToString(pair1.B);
+                    tbY1.Text = Convert.ToString(pair2.A);
+                    tbY2.Text = Convert.ToString(pair2.B);
+                    tbZ1.Text = Convert.ToString(pair3.A);
+                    tbZ2.Text = Convert.ToString(pair3.B);
+                }
+                else MessageBox.Show("!!!Введите удобоваримые значения!!!");
+            }
+        }
+
+        private void OneMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (rbTwoPairs.IsChecked == true)
+            {
+                if (Int32.TryParse(tbX1.Text, out int x1) == true && Int32.TryParse(tbX2.Text, out int x2) == true && Int32.TryParse(tbY1.Text, out int y1) == true && Int32.TryParse(tbY2.Text, out int y2) == true)
+                {
+                    Pair pair1 = new Pair(x1, x2);
+                    Pair pair2 = new Pair(y1, y2);
+                    pair1--;
+                    pair2--;
+                    tbX1.Text = Convert.ToString(pair1.A);
+                    tbX2.Text = Convert.ToString(pair1.B);
+                    tbY1.Text = Convert.ToString(pair2.A);
+                    tbY2.Text = Convert.ToString(pair2.B);
+                }
+                else MessageBox.Show("!!!Введите удобоваримые значения!!!");
+            }
+            else if (rbThreePairs.IsChecked == true)
+            {
+                if (Int32.TryParse(tbX1.Text, out int x1) == true && Int32.TryParse(tbX2.Text, out int x2) == true && Int32.TryParse(tbY1.Text, out int y1) == true && Int32.TryParse(tbY2.Text, out int y2) == true && Int32.TryParse(tbZ1.Text, out int z1) == true && Int32.TryParse(tbZ2.Text, out int z2) == true)
+                {
+                    Pair pair1 = new Pair(x1, x2);
+                    Pair pair2 = new Pair(y1, y2);
+                    Pair pair3 = new Pair(z1, z2);
+                    pair1--;
+                    pair2--;
+                    pair3--;
+                    tbX1.Text = Convert.ToString(pair1.A);
+                    tbX2.Text = Convert.ToString(pair1.B);
+                    tbY1.Text = Convert.ToString(pair2.A);
+                    tbY2.Text = Convert.ToString(pair2.B);
+                    tbZ1.Text = Convert.ToString(pair3.A);
+                    tbZ2.Text = Convert.ToString(pair3.B);
+                }
+                else MessageBox.Show("!!!Введите удобоваримые значения!!!");
+            }
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("\rРабота с классами. Перегрузка методов.\nАртемкин М. ИСП-31 Практическая работа №4\n\n2. Создать класс Pair (пара чисел). Создать необходимые методы и свойства. Определить методы метод сложения полей и \r\nоперацию сложения пар (а, b) + (с, d) = (а + c, b + d). Создать перегруженные методы для увеличения полей на 1,\r\nсложения трех пар чисел.");
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void rbTwoPairs_Checked(object sender, RoutedEventArgs e)
+        {
+            tbZ1.IsEnabled = false;
+            tbZ2.IsEnabled = false;
+        }
+
+        private void rbThreePairs_Checked(object sender, RoutedEventArgs e)
+        {
+            tbZ1.IsEnabled = true;
+            tbZ2.IsEnabled = true;
+        }
+    }
+}
---
+++ Pract03.05/Pair.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Effects;
+
+// Создать класс Pair (пара чисел). Создать необходимые методы и свойства. Определить методы метод сложения полей и 
+// операцию сложения пар (а, b) + (с, d) = (а + c, b + d). Создать перегруженные методы для увеличения полей на 1,
+// сложения трех пар чисел.
+
+namespace Pract03._05
+{
+    class Pair
+    {
+        int _a;
+        int _b;
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+        /// <summary>
+        /// Пара чисел
+        /// </summary>
+        public Pair()
+        {
+            A = 1;
+            B = 1;
+        }
+        /// <summary>
+        /// Пара чисел с параметрами
+        /// </summary>
+        /// <param name="a">Параметр 1</param>
+        /// <param name="b">Параметр 2</param>
+        public Pair(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        /// <summary>
+        /// Сумма двух пар
+        /// </summary>
+        /// <param name="pair1">Пара 1</param>
+        /// <param name="pair2">Пара 2</param>
+        /// <returns></returns>
+        static public Pair Sum(Pair pair1, Pair pair2)
+        {
+            Pair pair3 = new Pair();
+            pair3.A = pair1.A + pair2.A;
+            pair3.B = pair1.B + pair2.B;
+            return pair3;
+        }
+        /// <summary>
+        /// Сумма трёх пар
+        /// </summary>
+        /// <param name="pair1">Пара 1</param>
+        /// <param name="pair2">Пара 2</param>
+        /// <param name="pair3">Пара 3</param>
+        /// <returns></returns>
+        static public Pair Sum(Pair pair1, Pair pair2, Pair pair3)
+        {
+            Pair pair4 = new Pair();
+            pair4.A = pair1.A + pair2.A + pair3.A;
+            pair4.B = pair1.B + pair2.B + pair3.B;
+            return pair4;
+        }
+
+        /// <summary>
+        /// Плюс 1
+        /// </summary>
+        public void AddOne()
+        {
+            _a++;
+            _b++;
+        }
+        /// <summary>
+        /// Плюс 1 к конкретной паре
+        /// </summary>
+        /// <param name="pair1">Конкретная пара</param>
+        static public void AddOne(ref Pair pair1)
+        {
+            pair1.A++;
+            pair1.B++;
+        }
+
+        public static Pair operator --(Pair pair1)
+        {
+            pair1.A--;
+            pair1.B--;
+            return pair1;
+        }
+    }
+}
